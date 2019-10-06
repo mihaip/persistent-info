c439707c8dcce6ba5ab6d5de1d8a01c97041b173
Mihai Parparita <mihai@persistent.info>
2019-10-06 16:56:48 -0700

    Basic Prettier integration

--- homepage/pages/projects.js
+++ homepage/pages/projects.js
@@ -1,18 +1,16 @@
-import React from 'react'
-import Head from 'next/head'
-import Nav from '../components/nav'
+import React from "react";
+import Head from "next/head";
+import Nav from "../components/nav";
 
 const Projects = () => (
-  <div>
-    <Head>
-      <title>Projects</title>
-      <link rel='icon' href='/static/favicon.ico' />
-    </Head>
+    <div>
+        <Head>
+            <title>Projects</title>
+            <link rel="icon" href="/static/favicon.ico" />
+        </Head>
+        <Nav />
+        TODO
+    </div>
+);
 
-    <Nav />
-
-    TODO
-  </div>
-)
-
-export default Projects
+export default Projects;
